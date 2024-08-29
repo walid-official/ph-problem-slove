6d4dd4c9b0cd406a383f7a53dedd2ed699c471b7
walid hasan <walid.official8@gmail.com>
2024-08-29 12:48:29 +0600

    problem-2 is solved

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-// let orange = 300;
-// let apple = 400;
+let orange = 300;
+let apple = 400;
+let total = orange + apple;
+let customer = 1000;
+let shopKipper = customer - total;
+
+console.log(shopKipper);
+
+// let orange = parseInt(prompt('Enter Your Number :'));
+// let apple = parseInt(prompt('Enter Your Number :'));
+// let customer = parseInt(prompt('Enter Your Number :'));
+
 // let total = orange + apple;
-// let customer = 1000;
 // let shopKipper = customer - total;
 
 // console.log(shopKipper);
 
-let orange = parseInt(prompt('Enter Your Number :'));
-let apple = parseInt(prompt('Enter Your Number :'));
-let customer = parseInt(prompt('Enter Your Number :'));
 
-let total = orange + apple;
-let shopKipper = customer - total;
+let Mathematics, Biology, Chemistry, Physics, Bangla;
 
-console.log(shopKipper);
+Mathematics = 75.25;
+Biology = 65;
+Chemistry = 80; 
+Physics = 35.45;
+Bangla = 99.50;
+total = Mathematics + Biology + Chemistry + Physics + Bangla;
+const average = total / 5;
+console.log(average.toFixed(2));
