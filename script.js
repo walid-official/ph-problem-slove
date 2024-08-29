1f2de89c270e50ed1769803c77add6f2f8cf04ad
walid hasan <walid.official8@gmail.com>
2024-08-29 12:33:43 +0600

    problem1 is solved

---
+++ script.js
@@ -0,0 +1,16 @@
+// let orange = 300;
+// let apple = 400;
+// let total = orange + apple;
+// let customer = 1000;
+// let shopKipper = customer - total;
+
+// console.log(shopKipper);
+
+let orange = parseInt(prompt('Enter Your Number :'));
+let apple = parseInt(prompt('Enter Your Number :'));
+let customer = parseInt(prompt('Enter Your Number :'));
+
+let total = orange + apple;
+let shopKipper = customer - total;
+
+console.log(shopKipper);
